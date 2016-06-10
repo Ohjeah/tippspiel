f377c46a5aa32f4f62e3cfe9e0993fad959b315c
mq <mquade@uni-potsdam.de>
2016-06-10 15:04:20 +0200

    index

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-git pull origin master
+git pull origin gh-pages
 python tippspiel.py
 git add saure_gurke.pkl standings.png
 git commit -m "neue standings"
-git push origin master
+git push origin gh-pages
