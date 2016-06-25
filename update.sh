7037f9afaf3e1129343ec0b71334171b3c6791c4
mq <mquade@uni-potsdam.de>
2016-06-25 23:34:50 +0200

    neue standings

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 git pull origin gh-pages
 python tippspiel.py
-git add standings.png standings_vs_time.png ro*/results.txt index.html
+git add standings*.png ro*/results.txt index.html
 git commit -m "neue standings"
 git push origin gh-pages
